bf6ee341c5ffa5994dcfa8b81a7de3b6efe4ed77
Kiran <saikiranvinnakota059@gmail.com>
2024-09-12 23:12:17 +0530

    Default functions

--- delete_old_log_files.sh
+++ delete_old_log_files.sh
@@ -13,8 +13,8 @@ Files=$(find $Source_Dir -name "*.log" -mtime +14)
 
 echo "Files:$Files"
 
-while IFS= read -r line # IFS = Internal field seperator, empty it will ignore while space.-r is for not to ignore special charecters like /
+while IFS= read -r file # IFS = Internal field seperator, empty it will ignore while space.-r is for not to ignore special charecters like /
 do 
-    echo "Deleting line : $line"
-    rm -rf $line
+    echo "Deleting line : $file"
+    rm -rf $file
 done <<< $Files
